93185c1a814308e7d8e169772cfac3202177c5ce
Alvin Ng <ngalvin93@gmail.com>
2019-11-13 23:02:56 -0600

    complete

--- rectangle/index.js
+++ rectangle/index.js
@@ -1,29 +1,34 @@
-const { createStore } = Redux
-const { subscribe, dispatch, getState } = createStore(reducer)
+const { createStore } = Redux;
+const { subscribe, dispatch, getState } = createStore(reducer);
 
 subscribe(() => {
-  ReactDOM.render(<App rectangle={getState()} />, document.getElementById('rectangleContainer'))
-})
+    ReactDOM.render(<App rectangle={getState()} />, document.getElementById('rectangleContainer'))
+});
 
-dispatch({ type: null }) // Here we're making a call to dispatch() - this triggers the first render.
+dispatch({type: null}); // Here we're making a call to dispatch() - this triggers the first render.
 
-// Write DOM event listeners here, make them dispatch actions to the Redux store
-var dimensions = ['height', 'width']
+// Write DOM event listeners here, make them dispatch actions to the Redux store 
+var dimensions = ['height', 'width'];
 dimensions.forEach(dimension => {
-  document.getElementById(dimension).addEventListener('click', e => {
-    dispatch({
-      type: 'SIZE',
-      dimension
+    document.getElementById(dimension).addEventListener('click', e => {
+        dispatch({
+            type: 'SIZE',
+            dimension
+        })
     })
-  })
 })
 
 var colors = ['red', 'blue']
 colors.forEach(color => {
-  document.getElementById(color).addEventListener('click', e => {
-    dispatch({
-      type: 'COLOR',
-      color
+    document.getElementById(color).addEventListener('click', e => {
+        dispatch({
+            type: 'COLOR',
+            color
+        })
     })
-  })
 })
+
+const resetBtn = document.getElementById('reset')
+resetBtn.addEventListener('click', (e)=> dispatch({
+    type: 'RESET'
+}))
--- rectangle/reducer.js
+++ rectangle/reducer.js
@@ -1,23 +1,27 @@
 const initialState = {
   width: 100,
   height: 200,
-  color: '#FF00FF'
-}
+  color: "#FF00FF"
+};
 
 const reducer = (state = initialState, action) => {
   // Handle actions here - make sure you don't mutate the state!
-  const { type, dimension, color } = action
+  const { type, dimension, color } = action;
   // ACTION 1 - Increment dimension by 10
-  if (type == 'SIZE') {
-    return {
-      ...state,
-      [dimension]: state[dimension] + 10
-    }
+  if (type == "SIZE") {
+      return {
+          ...state,
+          [dimension]: state[dimension] + 10
+      }
   }
   // ACTION 2 - Change the color
-  if (type == 'COLOR') {
-    return { ...state, color }
+  else if (type == "COLOR") {
+      return { ...state, color }
+  }
+  // ACTION 3 - Reset color
+  else if (type === "RESET") {
+      return initialState
   }
 
-  return state
+  return state;
 }
